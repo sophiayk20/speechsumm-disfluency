dbe0686eaf39660ba3064d4b06081e0890b451d3
syk2021 <sophia.kang@yale.edu>
2025-05-05 13:10:57 -0400

    Fixing indentation issues

--- generate/repetitions/both_speakers.py
+++ generate/repetitions/both_speakers.py
@@ -1,166 +1,153 @@
-from datasets import load_dataset
+from datasets import load_dataset, Dataset
 from tqdm import tqdm
 from nltk.tokenize import sent_tokenize, word_tokenize
 from typing import Literal
 from itertools import zip_longest
-from python_files.disfluency_generation import LARD # should be in topic-controllable-summarization directory
+from python_files.disfluency_generation import LARD  # should be in topic-controllable-summarization directory
 import re
 import os
 import shutil
-shutil.rmtree('/content/drive/MyDrive/ling384/repetitions') # remove this folder in case it was already created
 
+# Remove this folder in case it was already created
+shutil.rmtree('/content/drive/MyDrive/ling384/repetitions', ignore_errors=True)
+
+# Load test dataset
 dataset = load_dataset("knkarthick/dialogsum", split='test')
 
-speaker_monologues = {} # 'train_0': {'#Person1#': ['Hi, Mr. Smith. I'm Doctor Hawkins. Why are you here today?', 'Yes, well, you haven't had one for 5 years. You should have one every year.'],
-                        #             '#Person2#': []}
+# Collect speaker monologues
+speaker_monologues = {}
 for instance in tqdm(dataset):
-  instance_id = instance['id']
-  speaker_monologues[instance_id] = {} # init dict
-  dialogues = instance['dialogue'].split('\n')
-  for dialogue in dialogues:
-    # max split at 1 because 17:05 is in text
-    speaker_identity, sentences = dialogue.split(':', 1)
-    sentences = sentences.strip() # sentences is a string
-    if speaker_identity not in speaker_monologues[instance_id].keys():
-      speaker_monologues[instance_id][speaker_identity] = []
-    speaker_monologues[instance_id][speaker_identity].append(sentences)
+    instance_id = instance['id']
+    speaker_monologues[instance_id] = {}
+    dialogues = instance['dialogue'].split('\n')
+    for dialogue in dialogues:
+        speaker_identity, sentences = dialogue.split(':', 1)
+        sentences = sentences.strip()
+        if speaker_identity not in speaker_monologues[instance_id]:
+            speaker_monologues[instance_id][speaker_identity] = []
+        speaker_monologues[instance_id][speaker_identity].append(sentences)
 
-print(len(speaker_monologues.keys()))
+print(len(speaker_monologues.keys()))  # Should be 1500
 
-# instantiate LARD object
+# Instantiate LARD
 lard = LARD()
 
 person_ids = ['#Person1#', '#Person2#']
-global_turn_flag = {person_id:False for person_id in person_ids} 
+global_turn_flag = {person_id: False for person_id in person_ids}
+
 
 def process_turn(person_id, person_turns, mode, BASE_FOLDER, repetition_degree=2):
-  global person_ids, global_turn_flag
+    global person_ids, global_turn_flag
 
-  running = ""
+    running = ""
 
-  with open(f"/{BASE_FOLDER}/repetition_{mode}_stat.txt", "a") as f:
-    person_turns_sentences = sent_tokenize(person_turns)
+    with open(f"/{BASE_FOLDER}/repetition_{mode}_stat.txt", "a") as f:
+        person_turns_sentences = sent_tokenize(person_turns)
 
-    # this turn flag is unique to this turn of person hence not global
-    turn_flag=False
-    disfluency_count = 0
-    speaker_running = []
+        turn_flag = False
+        disfluency_count = 0
+        speaker_running = []
 
-    for person_sentence in person_turns_sentences:
-      word_rep = word_tokenize(person_sentence)
-      string_rep = ' '.join(wp for wp in word_rep)
+        for person_sentence in person_turns_sentences:
+            word_rep = word_tokenize(person_sentence)
+            string_rep = ' '.join(wp for wp in word_rep)
 
-      # if we already created for this turn
-      if mode == "ATOS" and turn_flag:
-        speaker_running.append(person_sentence)
-        continue
-      if mode in ["OTOS", "OTAS"] and global_turn_flag[person_id] == True:
-        speaker_running.append(person_sentence)
-        continue
-      
-    disfluency = lard.create_repetitions(string_rep, repetition_degree)
+            if mode == "ATOS" and turn_flag:
+                speaker_running.append(person_sentence)
+                continue
+            if mode in ["OTOS", "OTAS"] and global_turn_flag[person_id]:
+                speaker_running.append(person_sentence)
+                continue
 
-    # if disfluency can be generated
-    if disfluency[0]:
-      tokens = disfluency[0].split()
-      rejoined_sentence = ' '.join(tokens)
+            disfluency = lard.create_repetitions(string_rep, repetition_degree)
 
-      # regular expression to handle all punctuation marks
-      # This ensures that punctuation is attached directly to the preceding word without extra spaces
-      rejoined_sentence = re.sub(r'\s([.,!?;])', r'\1', rejoined_sentence)
+            if disfluency[0]:
+                tokens = disfluency[0].split()
+                rejoined_sentence = ' '.join(tokens)
+                rejoined_sentence = re.sub(r'\s([.,!?;])', r'\1', rejoined_sentence)
+                rejoined_sentence = re.sub(r"\sn't", "n't", rejoined_sentence)
+                speaker_running.append(rejoined_sentence)
+                disfluency_count += 1
 
-      # Handle cases like "n't" contractions to avoid spacing issues
-      rejoined_sentence = re.sub(r"\sn't", "n't", rejoined_sentence)
-      speaker_running.append(rejoined_sentence)
+                if mode == "ATOS":
+                    turn_flag = True
+            else:
+                speaker_running.append(person_sentence)
 
-      disfluency_count += 1
+        running += f"{person_id}: "
+        running += " ".join(sent for sent in speaker_running)
+        running += "\n"
 
-      # if all turn one sentence, should be set to True
-      if mode == "ATOS":
-        turn_flag=True
-    # disfluency cannot be generated, add original sentence
-    else:
-      speaker_running.append(person_sentence)
+        if disfluency_count > 0 and mode in ['OTAS', 'OTOS']:
+            global_turn_flag[person_id] = True
 
-    # create new data
-    running += f"{person_id}: "
-    running += " ".join(sent for sent in speaker_running)
-    running += "\n"
+        f.write(f"{disfluency_count}\n")
 
-    # if we added disfluency in turn, disable future generation (one turn covered)
-    if disfluency_count > 0 and mode in ['OTAS', 'OTOS']:
-      global_turn_flag[person_id]= True
+    return running
 
-    f.write(f"{disfluency_count}\n")
-
-  return running
 
 def generate_repetition_both_speakers(instance_id, dialogue_dict, mode=Literal['ATAS', 'OTAS', 'ATOS', 'OTOS'], repetition_degree=2):
-  """
-    Given dialogue dict, return disfluent dialogue in string form
-    Generates at least 1 repetition disfluency in each turn
-  """
-  person1_num_speaks = len(dialogue_dict['#Person1#'])
-  person2_num_speaks = len(dialogue_dict['#Person2#'])
-  #print(dialogue_dict)
+    """
+    Given a dialogue dict, return disfluent dialogue as a string.
+    Generates at least 1 repetition disfluency in each turn.
+    """
+    person1_num_speaks = len(dialogue_dict['#Person1#'])
+    person2_num_speaks = len(dialogue_dict['#Person2#'])
 
-  global global_turn_flag
+    global global_turn_flag
+    global_turn_flag = {person_id: False for person_id in person_ids}
 
-  global_turn_flag = {person_id: False for person_id in person_ids}
+    dialogue_running = ""
 
-  dialogue_running = ""
+    BASE_FOLDER = "/content/drive/MyDrive/ling384/repetitions/both_speakers"
+    os.makedirs(BASE_FOLDER, exist_ok=True)
+    OUTPUT_FOLDER = f"{BASE_FOLDER}/{mode}-output"
+    os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
-  BASE_FOLDER=f"/content/drive/MyDrive/ling384/repetitions/both_speakers"
-  os.makedirs(BASE_FOLDER, exist_ok=True)
-  OUTPUT_FOLDER=f"{BASE_FOLDER}/{mode}-output"
-  os.makedirs(OUTPUT_FOLDER, exist_ok=True)
+    for person1_turn, person2_turn in zip_longest(dialogue_dict[person_ids[0]], dialogue_dict[person_ids[1]]):
+        if person1_turn:
+            ret = process_turn(person_ids[0], person1_turn, mode, BASE_FOLDER, repetition_degree)
+            dialogue_running += ret + '\n'
 
-  # zip longest is needed because 1 speaker might speak for more times (not even)
-  for person1_turn, person2_turn in zip_longest(dialogue_dict[person_ids[0]], dialogue_dict[person_ids[1]]):
-    if person1_turn:
-      ret = process_turn(person_ids[0], person1_turn, mode, BASE_FOLDER, repetition_degree)
-      dialogue_running += ret
-      dialogue_running += '\n'
+        if person2_turn:
+            ret = process_turn(person_ids[1], person2_turn, mode, BASE_FOLDER, repetition_degree)
+            dialogue_running += ret + '\n'
 
-    if person2_turn:
-      ret = process_turn(person_ids[1], person2_turn, mode, BASE_FOLDER, repetition_degree)
-      dialogue_running += ret
-      dialogue_running += '\n'
-    
-  with open(f"{OUTPUT_FOLDER}/{instance_id}.txt", "w") as f:
-    f.write(dialogue_running)
+    with open(f"{OUTPUT_FOLDER}/{instance_id}.txt", "w") as f:
+        f.write(dialogue_running)
 
-  return dialogue_running
+    return dialogue_running
 
-#for MODE in ['ATAS', 'ATOS', 'OTAS', 'OTOS']:
-for MODE in ['OTOS']:
-  print(f"Generating for this mode.... {MODE}!!")
-  for instance_id in tqdm(speaker_monologues.keys()):
-    generate_repetition_both_speakers(instance_id, speaker_monologues[instance_id], mode=MODE, repetition_degree=2)
-  
-  disfluent_dialogues = []
-  dialogues= []
-  ids = []
-  summaries= []
 
-  for instance in tqdm(dataset):
-    ids.append(instance['id'])
-    summaries.append(instance['summary'])
-    with open(f"/content/drive/MyDrive/ling384/repetitions/both_speakers/{MODE}-output/{instance['id']}.txt", 'r') as f:
-      lines = f.readlines()
+# Main loop for generation and dataset creation
+for MODE in ['OTOS']:  # can be changed to other modes if needed
+    print(f"Generating for this mode.... {MODE}!!")
+    for instance_id in tqdm(speaker_monologues.keys()):
+        generate_repetition_both_speakers(instance_id, speaker_monologues[instance_id], mode=MODE, repetition_degree=2)
 
-    text = ''.join(lines).strip()
-    disfluent_dialogues.append(text)
-    dialogues.append(instance['dialogue'])
+    disfluent_dialogues = []
+    dialogues = []
+    ids = []
+    summaries = []
 
-  assert(len(dialogues) == len(disfluent_dialogues) == len(ids) == len(summaries) == 1500)
+    for instance in tqdm(dataset):
+        ids.append(instance['id'])
+        summaries.append(instance['summary'])
+        with open(f"/content/drive/MyDrive/ling384/repetitions/both_speakers/{MODE}-output/{instance['id']}.txt", 'r') as f:
+            lines = f.readlines()
 
-  data = {
-      'id': ids,
-      'dialogue': dialogues,
-      'disfluent_dialogue': disfluent_dialogues,
-      'summary': summaries,
-  }
+        text = ''.join(lines).strip()
+        disfluent_dialogues.append(text)
+        dialogues.append(instance['dialogue'])
 
-  dataset = Dataset.from_dict(data)
-  dataset.push_to_hub("sophiayk20/repetition-both-speakers", split=MODE)
+    assert len(dialogues) == len(disfluent_dialogues) == len(ids) == len(summaries) == 1500
+
+    data = {
+        'id': ids,
+        'dialogue': dialogues,
+        'disfluent_dialogue': disfluent_dialogues,
+        'summary': summaries,
+    }
+
+    dataset = Dataset.from_dict(data)
+    dataset.push_to_hub("sophiayk20/repetition-both-speakers", split=MODE)
--- generate/repetitions/one_speaker.py
+++ generate/repetitions/one_speaker.py
@@ -1,189 +1,163 @@
-from datasets import load_dataset
+from datasets import load_dataset, Dataset
 from tqdm import tqdm
 from nltk.tokenize import sent_tokenize, word_tokenize
 from typing import Literal
 from itertools import zip_longest
-from python_files.disfluency_generation import LARD # should be in topic-controllable-summarization directory
+from python_files.disfluency_generation import LARD  # should be in topic-controllable-summarization directory
 import re
 import os
 import random
 import shutil
-shutil.rmtree('/content/drive/MyDrive/ling384/repetitions') # remove this folder in case it was already created
 
+# Clean up previous output directory if exists
+shutil.rmtree('/content/drive/MyDrive/ling384/repetitions', ignore_errors=True)
+
+# Load dataset
 dataset = load_dataset("knkarthick/dialogsum", split='test')
 
-speaker_monologues = {} # 'train_0': {'#Person1#': ['Hi, Mr. Smith. I'm Doctor Hawkins. Why are you here today?', 'Yes, well, you haven't had one for 5 years. You should have one every year.'],
-                        #             '#Person2#': []}
+# Prepare speaker monologues
+speaker_monologues = {}
 for instance in tqdm(dataset):
-  instance_id = instance['id']
-  speaker_monologues[instance_id] = {} # init dict
-  dialogues = instance['dialogue'].split('\n')
-  for dialogue in dialogues:
-    # max split at 1 because 17:05 is in text
-    speaker_identity, sentences = dialogue.split(':', 1)
-    sentences = sentences.strip() # sentences is a string
-    if speaker_identity not in speaker_monologues[instance_id].keys():
-      speaker_monologues[instance_id][speaker_identity] = []
-    speaker_monologues[instance_id][speaker_identity].append(sentences)
+    instance_id = instance['id']
+    speaker_monologues[instance_id] = {}
+    dialogues = instance['dialogue'].split('\n')
+    for dialogue in dialogues:
+        speaker_identity, sentences = dialogue.split(':', 1)
+        sentences = sentences.strip()
+        if speaker_identity not in speaker_monologues[instance_id]:
+            speaker_monologues[instance_id][speaker_identity] = []
+        speaker_monologues[instance_id][speaker_identity].append(sentences)
 
-print(len(speaker_monologues.keys())) # this should be 1500!!! (length of test set)
+print(len(speaker_monologues.keys()))  # Should be 1500 (length of test set)
 
-# instantiate LARD object
+# Instantiate LARD object
 lard = LARD()
 
 person_ids = ['#Person1#', '#Person2#']
-global_turn_flag = {person_id:False for person_id in person_ids} 
+global_turn_flag = {person_id: False for person_id in person_ids}
+
 
 def process_turn(person_id, person_turns, mode, BASE_FOLDER, repetition_degree=2):
-  global person_ids, global_turn_flag
+    global person_ids, global_turn_flag
 
-  running = ""
+    running = ""
 
-  with open(f"/{BASE_FOLDER}/repetition_{mode}_stat.txt", "a") as f:
-    person_turns_sentences = sent_tokenize(person_turns)
+    with open(f"/{BASE_FOLDER}/repetition_{mode}_stat.txt", "a") as f:
+        person_turns_sentences = sent_tokenize(person_turns)
 
-    # this turn flag is unique to this turn of person hence not global
-    turn_flag=False
-    disfluency_count = 0
-    speaker_running = []
+        turn_flag = False
+        disfluency_count = 0
+        speaker_running = []
 
-    for person_sentence in person_turns_sentences:
-      word_rep = word_tokenize(person_sentence)
-      string_rep = ' '.join(wp for wp in word_rep)
+        for person_sentence in person_turns_sentences:
+            word_rep = word_tokenize(person_sentence)
+            string_rep = ' '.join(wp for wp in word_rep)
 
-      # if we already created for this turn
-      if mode == "ATOS" and turn_flag:
-        speaker_running.append(person_sentence)
-        continue
-      if mode in ["OTOS", "OTAS"] and global_turn_flag[person_id] == True:
-        speaker_running.append(person_sentence)
-        continue
-      
-    disfluency = lard.create_repetitions(string_rep, repetition_degree)
+            if mode == "ATOS" and turn_flag:
+                speaker_running.append(person_sentence)
+                continue
+            if mode in ["OTOS", "OTAS"] and global_turn_flag[person_id]:
+                speaker_running.append(person_sentence)
+                continue
 
-    # if disfluency can be generated
-    if disfluency[0]:
-      tokens = disfluency[0].split()
-      rejoined_sentence = ' '.join(tokens)
+            disfluency = lard.create_repetitions(string_rep, repetition_degree)
 
-      # regular expression to handle all punctuation marks
-      # This ensures that punctuation is attached directly to the preceding word without extra spaces
-      rejoined_sentence = re.sub(r'\s([.,!?;])', r'\1', rejoined_sentence)
+            if disfluency[0]:
+                tokens = disfluency[0].split()
+                rejoined_sentence = ' '.join(tokens)
+                rejoined_sentence = re.sub(r'\s([.,!?;])', r'\1', rejoined_sentence)
+                rejoined_sentence = re.sub(r"\sn't", "n't", rejoined_sentence)
+                speaker_running.append(rejoined_sentence)
+                disfluency_count += 1
+                if mode == "ATOS":
+                    turn_flag = True
+            else:
+                speaker_running.append(person_sentence)
 
-      # Handle cases like "n't" contractions to avoid spacing issues
-      rejoined_sentence = re.sub(r"\sn't", "n't", rejoined_sentence)
-      speaker_running.append(rejoined_sentence)
+        running += f"{person_id}: "
+        running += " ".join(sent for sent in speaker_running)
+        running += "\n"
 
-      disfluency_count += 1
+        if disfluency_count > 0 and mode in ['OTAS', 'OTOS']:
+            global_turn_flag[person_id] = True
 
-      # if all turn one sentence, should be set to True
-      if mode == "ATOS":
-        turn_flag=True
-    # disfluency cannot be generated, add original sentence
-    else:
-      speaker_running.append(person_sentence)
+        f.write(f"{disfluency_count}\n")
 
-    # create new data
-    running += f"{person_id}: "
-    running += " ".join(sent for sent in speaker_running)
-    running += "\n"
+    return running
 
-    # if we added disfluency in turn, disable future generation (one turn covered)
-    if disfluency_count > 0 and mode in ['OTAS', 'OTOS']:
-      global_turn_flag[person_id]= True
-
-    f.write(f"{disfluency_count}\n")
-
-  return running
 
 def generate_repetition_one_speaker(instance_id, dialogue_dict, mode=Literal['ATAS', 'OTAS', 'ATOS', 'OTOS'], repetition_degree=2):
-  """
-    Given dialogue dict, return disfluent dialogue in string form
-    Generates at least 1 repetition disfluency in each turn
-  """
-  person1_num_speaks = len(dialogue_dict['#Person1#'])
-  person2_num_speaks = len(dialogue_dict['#Person2#'])
-  #print(dialogue_dict)
-  random.seed(42)
+    person1_num_speaks = len(dialogue_dict['#Person1#'])
+    person2_num_speaks = len(dialogue_dict['#Person2#'])
 
-  global global_turn_flag
+    random.seed(42)
+    global global_turn_flag
+    global_turn_flag = {person_id: False for person_id in person_ids}
 
-  global_turn_flag = {person_id: False for person_id in person_ids}
+    dialogue_running = ""
 
-  dialogue_running = ""
+    BASE_FOLDER = "/content/drive/MyDrive/ling384/repetitions/one_speaker"
+    os.makedirs(BASE_FOLDER, exist_ok=True)
+    OUTPUT_FOLDER = f"{BASE_FOLDER}/{mode}-output"
+    os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
-  BASE_FOLDER=f"/content/drive/MyDrive/ling384/repetitions/one_speaker"
-  os.makedirs(BASE_FOLDER, exist_ok=True)
-  OUTPUT_FOLDER=f"{BASE_FOLDER}/{mode}-output"
-  os.makedirs(OUTPUT_FOLDER, exist_ok=True)
+    for person1_turn, person2_turn in zip_longest(dialogue_dict[person_ids[0]], dialogue_dict[person_ids[1]]):
+        repetition_index = random.choice([0, 1])
 
-  # for each set of sentences that a pereson says in a single turn
-  # person1_turns: 'Hi, Mr. Smith. I'm Doctor Hawkins. Why are you here today?'
-  for person1_turn, person2_turn in zip_longest(dialogue_dict[person_ids[0]], dialogue_dict[person_ids[1]]):
-      repetition_index = random.choice([0, 1])
+        if person1_turn:
+            if repetition_index == 0:
+                ret = process_turn(person_ids[0], person1_turn, mode, BASE_FOLDER)
+                dialogue_running += ret + '\n'
+            else:
+                dialogue_running += f"{person_ids[0]}: {person1_turn}\n"
 
-      if person1_turn:
-        # if this person repeats
-        if repetition_index == 0:
-          ret = process_turn(person_ids[0], person1_turn, mode, BASE_FOLDER)
-          dialogue_running += ret
-          dialogue_running += '\n'
-        # else, other person repeats so keep original sentence
-        else:
-          dialogue_running += f"{person_ids[0]}: "
-          dialogue_running += person1_turn
-          dialogue_running += '\n'
-      
-      if person2_turn:
-        # if this person repeats
-        if repetition_index == 1:
-          ret = process_turn(person_ids[1], person2_turn, mode, BASE_FOLDER)
-          dialogue_running += ret
-          dialogue_running += '\n'
-        else:
-          dialogue_running += f"{person_ids[1]}: "
-          dialogue_running += person2_turn
-          dialogue_running += '\n'
+        if person2_turn:
+            if repetition_index == 1:
+                ret = process_turn(person_ids[1], person2_turn, mode, BASE_FOLDER)
+                dialogue_running += ret + '\n'
+            else:
+                dialogue_running += f"{person_ids[1]}: {person2_turn}\n"
 
-  with open(f"{OUTPUT_FOLDER}/{instance_id}.txt", "w") as f:
-    f.write(dialogue_running)
-  
-  return dialogue_running
+    with open(f"{OUTPUT_FOLDER}/{instance_id}.txt", "w") as f:
+        f.write(dialogue_running)
 
-#for MODE in ['ATAS', 'ATOS', 'OTAS', 'OTOS']:
+    return dialogue_running
+
+
+# Generate disfluencies and push dataset to hub
 for MODE in ['OTOS']:
-  print(f"Generating for this mode.... {MODE}!!")
-  for instance_id in tqdm(speaker_monologues.keys()):
-    generate_repetition_one_speaker(instance_id, speaker_monologues[instance_id], mode=MODE, repetition_degree=2)
+    print(f"Generating for this mode.... {MODE}!!")
+    for instance_id in tqdm(speaker_monologues.keys()):
+        generate_repetition_one_speaker(instance_id, speaker_monologues[instance_id], mode=MODE, repetition_degree=2)
 
-  disfluent_dialogues = []
-  dialogues= []
-  ids = []
-  summaries= []
+    disfluent_dialogues = []
+    dialogues = []
+    ids = []
+    summaries = []
 
-  for instance in tqdm(dataset):
-    ids.append(instance['id'])
-    summaries.append(instance['summary'])
-    with open(f"/content/drive/MyDrive/ling384/repetitions/one_speaker/{MODE}-output/{instance['id']}.txt", 'r') as f:
-      lines = f.readlines()
+    for instance in tqdm(dataset):
+        ids.append(instance['id'])
+        summaries.append(instance['summary'])
+        with open(f"/content/drive/MyDrive/ling384/repetitions/one_speaker/{MODE}-output/{instance['id']}.txt", 'r') as f:
+            lines = f.readlines()
 
-    text = ''.join(lines).strip()
-    disfluent_dialogues.append(text)
-    dialogues.append(instance['dialogue'])
+        text = ''.join(lines).strip()
+        disfluent_dialogues.append(text)
+        dialogues.append(instance['dialogue'])
 
-  print(f"Length of dialogues: {len(dialogues)}")
-  print(f"Length of disfluent dialogues: {len(disfluent_dialogues)}")
-  print(f"Length of ids: {len(ids)}")
-  print(f"Length of summaries: {len(summaries)}")
+    print(f"Length of dialogues: {len(dialogues)}")
+    print(f"Length of disfluent dialogues: {len(disfluent_dialogues)}")
+    print(f"Length of ids: {len(ids)}")
+    print(f"Length of summaries: {len(summaries)}")
 
-  assert(len(dialogues) == len(disfluent_dialogues) == len(ids) == len(summaries) == 1500)
+    assert len(dialogues) == len(disfluent_dialogues) == len(ids) == len(summaries) == 1500
 
-  data = {
-      'id': ids,
-      'dialogue': dialogues,
-      'disfluent_dialogue': disfluent_dialogues,
-      'summary': summaries,
-  }
+    data = {
+        'id': ids,
+        'dialogue': dialogues,
+        'disfluent_dialogue': disfluent_dialogues,
+        'summary': summaries,
+    }
 
-  dataset = Dataset.from_dict(data)
-  dataset.push_to_hub("sophiayk20/repetition-one-speaker", split=MODE)
+    dataset = Dataset.from_dict(data)
+    dataset.push_to_hub("sophiayk20/repetition-one-speaker", split=MODE)
